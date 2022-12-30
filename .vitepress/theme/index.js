c7203b33a2e1425a56b7c5408e055b4b410ee881
Peter Fox <peter.fox@peterfox.me>
2022-12-30 18:54:44 +0000

    Fix the default theme problems

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,14 +1,8 @@
 // .vitepress/theme/index.js
-import DefaultTheme from 'vitepress/theme'
+
+import DefaultTheme from './ylsideas'
 import './docs.css'
 
 export default {
     ...DefaultTheme,
-    enhanceApp(ctx) {
-        // extend default theme custom behaviour.
-        DefaultTheme.enhanceApp(ctx)
-
-        // register your custom global components
-        ctx.app.component('MyGlobalComponent' /* ... */)
-    }
 }
